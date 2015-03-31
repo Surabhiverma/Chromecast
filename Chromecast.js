2e35092499ef613f3b5524fc6df8c933c9e35d8f
Surabhiverma <surabhiverma4@gmail.com>
2015-03-31 23:29:15 +0530

    Java script added

---
+++ Chromecast.js
@@ -0,0 +1,122 @@
+<script type="text/javascript" src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js"> 
+var sessin = null;
+function ready()
+{
+if (!chrome.cast || !chrome.cast.isAvailable) 
+{
+  
+	console.log('Initialization delay...');
+	setTimeout(initializeCastApi, CAST_API_INITIALIZATION_DELAY);
+
+}
+}
+
+function initializeCastApi() 
+{
+    
+// request session
+  
+var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
+);
+  
+var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+    sessionListener,
+    receiverListener);
+
+  
+chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
+
+}
+
+
+function sessionListener(e) 
+{
+session = e;
+console.log('New session');
+if (session.media.length != 0) 
+{
+console.log('Found ' + session.media.length +'sessions.');
+}
+}
+
+function receiverListener(e) 
+{
+if( e === 'available' ) 
+{
+console.log("Chromecast was found on the network.");
+}
+else 
+{
+console.log("There are no Chromecasts available.");
+}
+}
+
+function onInitSuccess() 
+{
+console.log("Initialization succeeded");
+}
+
+function onInitError() 
+{
+console.log("Initialization failed");
+}
+
+function launchApp() 
+{
+console.log("Launching the Chromecast App...");
+chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+}
+
+function onRequestSessionSuccess(e) 
+{
+console.log("Successfully created session: " + e.sessionId);
+session = e;
+loadMedia();
+}
+
+function onLaunchError() 
+{
+console.log("Error connecting to the Chromecast.");
+}
+
+function loadMedia() 
+{
+if (!session) 
+{
+console.log("No session.");
+return;
+}
+var mediaInfo = new chrome.cast.media.MediaInfo('http://i.imgur.com/IFD14.jpg');
+mediaInfo.contentType = 'image/jpg';
+var request = new chrome.cast.media.LoadRequest(mediaInfo);
+request.autoplay = true;
+session.loadMedia(request, onLoadSuccess, onLoadError);
+}
+
+function onLoadSuccess() 
+{
+console.log('Successfully loaded image.');
+}
+
+function onLoadError() 
+{
+console.log('Failed to load image.');
+}
+
+function stopApp() 
+{
+session.stop(onStopAppSuccess, onStopAppError);
+}
+
+function onStopAppSuccess() 
+{
+console.log('Successfully stopped app.');
+}
+
+function onStopAppError() 
+{
+console.log('Error stopping app.');
+}
+
+
+</script>
