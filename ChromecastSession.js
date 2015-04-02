8f042688959e7d8c3bc680f059436475f180cd25
Surabhiverma <surabhiverma4@gmail.com>
2015-04-02 21:13:27 +0530

    Update ChromecastSession.js

--- ChromecastSession.js
+++ ChromecastSession.js
@@ -103,8 +103,9 @@ if (!session)
 console.log("No session.");
 return;
 }
-var mediaInfo = new chrome.cast.media.MediaInfo('http://i.imgur.com/IFD14.jpg');
-mediaInfo.contentType = 'image/jpg';
+new chrome.cast.media.MediaInfo('http://commondatastorage.googleapis.com/
+                gtv-videos-bucket/ED_1280.mp4');
+        mediaInfo.contentType = 'video/mp4';
 var request = new chrome.cast.media.LoadRequest(mediaInfo);
 request.autoplay = true;
 session.loadMedia(request, onLoadSuccess, onLoadError);
